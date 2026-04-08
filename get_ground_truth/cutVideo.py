8692f7e281f3b74ae3866ce5ab9be7f6735a9074
Sophia Celine <sophiaceline@usp.br>
2026-04-08 19:10:11 -0300

    add preliminary results

--- get_ground_truth/cutVideo.py
+++ get_ground_truth/cutVideo.py
@@ -5,9 +5,9 @@ Opens an .avi video and crops it between a start frame and an end frame.
 import cv2
 
 input_path = r"C:\Users\Sophia\Documents\20260309_Coleta Vinicius\20260309_Coleta Vinicius\video004_corrected.avi"
-output_path = r"C:\Users\Sophia\Documents\20260309_Coleta Vinicius\20260309_Coleta Vinicius\video004_cropped.avi"
+output_path = r"C:\Users\Sophia\Documents\20260309_Coleta Vinicius\20260309_Coleta Vinicius\video004_cropped2.avi"
 start_frame = 249 
-end_frame = 6249
+end_frame = 3000
 
 cap = cv2.VideoCapture(input_path)
 print("Old frame count:", int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
