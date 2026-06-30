8f877a09cdd3b223def15981755f250bec2e2f2c
Sophia Celine <sophiaceline@usp.br>
2026-06-30 14:49:17 -0300

    add frame crop offset

--- acquisition/record.py
+++ acquisition/record.py
@@ -7,15 +7,24 @@ import cv2
 import neoapi
 
 # Camera configuration variables
-OUTPUT_FILEPATH = 'outpy.avi'
+OUTPUT_FILEPATH = 'imgsize2.avi'
+ROI_IMAGE_FILEPATH = 'initial_frame.png'
 CODEC = 'XVID'
 FRAME_RATE = 50
-FRAME_WIDTH = 1056
-FRAME_HEIGHT = 1076
+FRAME_WIDTH =  1056  # max is 2464
+FRAME_HEIGHT = 1076  # max is 2048
+FRAME_OFFSET_X = 704  # horizontal offset from the sensor origin, in pixels
+FRAME_OFFSET_Y = 486  # vertical offset from the sensor origin, in pixels
+# FRAME_WIDTH =  2464
+# FRAME_HEIGHT = 2048
+# FRAME_OFFSET_X = 0  # horizontal offset from the sensor origin, in pixels
+# FRAME_OFFSET_Y = 0  # vertical offset from the sensor origin, in pixels
 EXPOSURE_TIME_US = 19500
 PIXEL_FORMAT = 'BGR8'
 RECORD_DURATION_SEC = 5
 WINDOW_TITLE = 'Press ESC to stop recording'
+GAIN = 1
+SAVE_ROI = True
 
 
 def main():
@@ -39,9 +48,14 @@ def main():
         camera.f.ExposureTime.Set(EXPOSURE_TIME_US)
         camera.f.AcquisitionFrameRateEnable.value = True
         camera.f.AcquisitionFrameRate.value = FRAME_RATE
+        # camera.SetFeature("Gain", GAIN)
+
+        camera.f.Gain.value = GAIN
 
         camera.f.Width.Set(FRAME_WIDTH)
         camera.f.Height.Set(FRAME_HEIGHT)
+        camera.f.OffsetX.Set(FRAME_OFFSET_X)
+        camera.f.OffsetY.Set(FRAME_OFFSET_Y)
 
         width = int(camera.f.Width.value)
         height = int(camera.f.Height.value)
@@ -60,6 +74,10 @@ def main():
             if image is None:
                 print('Failed to capture frame', frame_index)
                 break
+
+            if SAVE_ROI and frame_index == 0:
+                cv2.imwrite(ROI_IMAGE_FILEPATH, image)
+                print(f'Saved initial frame to {ROI_IMAGE_FILEPATH}')
 
             cv2.imshow(WINDOW_TITLE, image)
             video_writer.write(image)
